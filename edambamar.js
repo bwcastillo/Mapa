3746c4185535ab3e35aa41bdac83b6be553127d4
bwcastillo <bwcastillo@uc.cl>
2018-04-02 16:20:52 -0300

    Agregué la descripción a los textos rellenos, eliminé archivos innecesarios, limpié un poco el código

--- edambamar.js
+++
@@ -1,9 +0,0 @@
-// JavaScript Document
-
-L.marker([-33.5088007,-70.7553773]).bindPopup("<b>Nombre actividad</b><br>	HUERTO ORGÁNICO<br><b>Lugar</b><br>	Biblioteca Municipal	<br><b>Dirección</b><br>	1921 Alberto Llona	<br><b>Encargado</b><br>	Cristian Osorio	<br><b>Fecha inicio</b><br>	20-Mar	<br><b>Horario</b><br>	Lunes 15:15 - 17:15	<br><b>Estado</b><br>	COMPLETADO	").openPopup().addTo(edAmMar);
-
-L.marker([	-33.5088007	,	-70.7553773	],{icon: L.AwesomeMarkers.icon({
-      icon: 'bell', 
-      prefix: 'glyphicon', 
-      markerColor: 'orange'})}).bindPopup("<b>Nombre actividad</b><br>	CONSTRUCCION SUSTENTABLE<br><b>Lugar</b><br>	Biblioteca Municipal	<br><b>Dirección</b><br>	1921 Alberto Llona	<br><b>Encargado</b><br>	Matias Chavez	<br><b>Fecha inicio</b><br>	20-Mar	<br><b>Horario</b><br>	Lunes 15:15 - 17:15	<br><b>Estado</b><br>	COMPLETADO	").openPopup().addTo(edAmMar);
-L.marker([	-33.473155	,	-70.756197	]).bindPopup("<b>Nombre actividad</b><br>	HIERBAS MEDICINALES<br>	<b>Lugar</b><br>	CESFAM Luis Ferrada	<br><b>Dirección</b><br> 5161 Tierra Fértil	<br><b>Encargado</b><br>	Jonatan Mora - Paulina Valenzuela	<br><b>Fecha inicio</b><br>	06-Mar	<br><b>Horario</b><br>	Jueves 15:30- 17:00	<br><b>Estado</b><br>	VIGENTE	"	).openPopup().addTo(edAmMar);
